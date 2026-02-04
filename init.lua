c384d3a491cdcadde959abb143177fc6e8f35b5d
Sondre Eide Vik <sondre.vik@spv.no>
2026-02-04 11:48:27 +0100

    chore: setup c# lsp

--- init.lua
+++ init.lua
@@ -597,6 +597,7 @@ require('lazy').setup({
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
+        omnisharp = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -885,7 +886,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
